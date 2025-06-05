1338173bc659c2e89692d1a25f3c8a4b9ff1cc6a
nekkochan0x0007 <nekkochan0x0007@tutamail.com>
2025-06-05 15:01:32 +0700

    Update

--- start_service.sh
+++ start_service.sh
@@ -2,4 +2,4 @@
 set -e
 cd /home/btldtdm1005/personal-ai-assistant
 source venv/bin/activate
-exec gunicorn --workers 2 --bind 127.0.0.1:5000 --timeout 120 --keep-alive 2 --max-requests 1000 --max-requests-jitter 50 fast_chatbot_api:app
+exec gunicorn --workers 2 --bind 127.0.0.1:5000 --timeout 650 --keep-alive 2 --max-requests 1000 --max-requests-jitter 50 fast_chatbot_api:app
